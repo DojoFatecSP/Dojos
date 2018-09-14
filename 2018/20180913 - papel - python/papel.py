e5aa7e69a3dc8fe5341fed9eedd5e66e91726522
DojoFatecSP <dojofatecsp@gmail.com>
2018-09-14 07:30:09 -0300

    Dojo 13/09/2018

---
+++ 2018/20180913 - papel - python/papel.py
@@ -0,0 +1,16 @@
+def solve(q, f, a):
+    chutes = 2 * f
+    jogadores = list(range(q))
+    jogadores.reverse()
+    jogador_inicial = jogadores.index(a)
+
+    # enquanto numero de jogadores não for 1
+    # calcular o aluno eliminado ********
+    # retirar o eliminado da lista
+    # iniciar o calculo do jogador inicial a partir do indice do eliminado - 1
+    # aumentar o chutes em 2
+
+
+
+
+    return 1
